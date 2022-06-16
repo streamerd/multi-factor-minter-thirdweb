da6d8645c8c9dc091bb0a6b532f9165bf5f4771f
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2022-06-16 12:49:16 +0200

    add steps card with spinners, on walletconnect and Minting  pages

--- components/SignIn.js
+++ components/SignIn.js
@@ -1,15 +1,27 @@
-import { useAddress, useDisconnect, useMetamask, useMintNFT, useNFTCollection } from "@thirdweb-dev/react";
-import { Box, Card, Text, Button, Image, Spinner, Footer, Paragraph } from 'grommet'
+import {
+  useAddress,
+  useDisconnect,
+  useMetamask,
+  useMintNFT,
+  useNFTCollection,
+} from "@thirdweb-dev/react";
+import {
+  Box,
+  Card,
+  Text,
+  Button,
+  Image,
+  Spinner,
+  Footer,
+  Paragraph,
+  ResponsiveContext,
+} from "grommet";
 import { useSession, signIn, signOut } from "next-auth/react";
 import React, { useState } from "react";
 import styles from "../styles/Theme.module.css";
 import Airdrop from "./Airdrop";
 
-
-import { Currency, Close, Checkmark } from 'grommet-icons'
-
-
-
+import { Currency, Close, Checkmark } from "grommet-icons";
 
 const ConnectWallet = () => {
   const address = useAddress();
@@ -17,30 +29,44 @@ const ConnectWallet = () => {
   const disconnectWallet = useDisconnect();
   const { data: session } = useSession();
 
-return (
-  <Box align="stretch" justify="center">
-    <Box align="center" justify="center" pad="large" gap="none">
-      <Box align="center" justify="center">
-        <Card align="stretch" justify="center" direction="column" pad="large" gap="small">
-          <Box align="center" justify="center" pad="xsmall" direction="column" gap="small">
-            <Text textAlign="center">
-              AUTHORIZED ACCESS ONLY
-            </Text>
-            <Text textAlign="center">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet elementum, laoreet egestas elit vitae 
-
-            </Text>
-          </Box>
-          <Box align="stretch" justify="center" pad="small" gap="small">
-            <Button label="Connect Wallet" size="medium" primary onClick={() => connectWithMetamask()} />
-            <Button label="Launch" active={false} disabled size="medium" />
-          </Box>
-        </Card>
+  return (
+    <Box align="stretch" justify="center">
+      <Box align="center" justify="center" pad="large" gap="none">
+        <Box align="center" justify="center">
+          <Card
+            align="stretch"
+            justify="center"
+            direction="column"
+            pad="large"
+            gap="small"
+          >
+            <Box
+              align="center"
+              justify="center"
+              pad="xsmall"
+              direction="column"
+              gap="small"
+            >
+              <Text textAlign="center">AUTHORIZED ACCESS ONLY</Text>
+              <Paragraph textAlign="center">
+                You need to connect your metamask wallet to Alpha mint.
+              </Paragraph>
+            </Box>
+            <Box align="stretch" justify="center" pad="small" gap="small">
+              <Button
+                label="Connect Wallet"
+                size="medium"
+                primary
+                onClick={() => connectWithMetamask()}
+              />
+              <Button label="Launch" active={false} disabled size="medium" />
+            </Box>
+          </Card>
+        </Box>
       </Box>
     </Box>
-  </Box>
-)
-}
+  );
+};
 
 const MintingInterfaceItem = () => {
   const address = useAddress();
@@ -49,21 +75,20 @@ const MintingInterfaceItem = () => {
   );
 
   async function mintNft() {
-  
-//    Ensure wallet connected
+    //    Ensure wallet connected
     // if (!address) {
     //   alert("Please reconnect your wallet to continue.");
     //   return;
     // }
-  
+
     // // Ensure correct network
     // if (isOnWrongNetwork) {
     //   switchNetwork(ChainId.Rinkeby);
     //   return;
     // }
-  
-    console.log("generating signature ...")
-  
+
+    console.log("generating signature ...");
+
     // Make a request to the API route to generate a signature for us to mint the NFT with
     const signature = await fetch(`/api/generate-signature`, {
       method: "POST",
@@ -72,14 +97,14 @@ const MintingInterfaceItem = () => {
         claimerAddress: address,
       }),
     });
-  
+
     // If the user meets the criteria to have a signature generated, we can use the signature
     // on the client side to mint the NFT from this client's wallet
     if (signature.status === 200) {
       const json = await signature.json();
       const signedPayload = json.signedPayload;
       const nft = await nftCollectionContract?.signature.mint(signedPayload);
-  
+
       // Show a link to view the NFT they minted
       alert(
         `Success 🔥  Check out your NFT here: https://testnets.opensea.io/assets/rinkeby/0xD93bEC957B531Ce2Ea6b86F0132ed8a8ae4ad533/${nft.id.toNumber()}`
@@ -93,35 +118,68 @@ const MintingInterfaceItem = () => {
 
   return (
     <Box align="stretch" justify="center" direction="column">
-      <Box align="stretch" justify="center" pad="medium" direction="row" gap="small">
+      <Box
+        align="stretch"
+        justify="center"
+        pad="medium"
+        direction="row"
+        gap="small"
+      >
         <Box align="stretch" justify="center" direction="column">
           <Card direction="column" justify="center" align="stretch">
             <Box align="center" justify="center" pad="small" direction="column">
-              <Image src="https://photos.smugmug.com/Pinnacles-May-2019/n-8KLNDR/i-bxkrqwL/0/1c7fa7f2/M/i-bxkrqwL-M.jpg" fill="horizontal" fit="cover" />
+              <Image
+                src="https://photos.smugmug.com/Pinnacles-May-2019/n-8KLNDR/i-bxkrqwL/0/1c7fa7f2/M/i-bxkrqwL-M.jpg"
+                fill="horizontal"
+                fit="cover"
+              />
             </Box>
-            <Box align="stretch" justify="between" direction="column" gap="small">
+            <Box
+              align="stretch"
+              justify="between"
+              direction="column"
+              gap="small"
+            >
               <Box align="start" justify="center" pad="small">
                 <Text textAlign="start" weight={"bolder"} margin="small">
                   SUMMERJAM
                 </Text>
-                <Text textAlign="center" size="xlarge" weight="bold" margin={"small"}>
+                <Text
+                  textAlign="center"
+                  size="xlarge"
+                  weight="bold"
+                  margin={"small"}
+                >
                   Metaverse has never been this delightful
                 </Text>
-                
               </Box>
               <Box align="center" justify="center" gap="small">
                 <Paragraph textAlign="center">
-                  Remarkable virtual craftsmanship meets ostentatious yet familiar design. 
+                  Remarkable virtual craftsmanship meets ostentatious yet
+                  familiar design.
                 </Paragraph>
 
                 <Paragraph>
-                Ingredients from a different dimension and extravagant hints of fruits suiting everyone&apos;s palate.
+                  Ingredients from a different dimension and extravagant hints
+                  of fruits suiting everyone&apos;s palate.
                 </Paragraph>
-                <Text size="small" alignSelf="center" weight="bold" margin={"small"}>
-                  Exclusive edition of 60 limited edition summer jams in three delightful varieties.
+                <Text
+                  size="small"
+                  alignSelf="center"
+                  weight="bold"
+                  margin={"small"}
+                >
+                  Exclusive edition of 60 limited edition summer jams in three
+                  delightful varieties.
                 </Text>
               </Box>
-              <Box align="center" justify="start" pad="small" direction="row" gap="small">
+              <Box
+                align="center"
+                justify="start"
+                pad="small"
+                direction="row"
+                gap="small"
+              >
                 <Box align="center" justify="start" direction="row">
                   <Currency size="large" />
                 </Box>
@@ -141,50 +199,77 @@ const MintingInterfaceItem = () => {
         </Box>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
 const MintingApprove = () => {
-  
-  return (                          
+  return (
     <Box align="stretch" justify="center">
       <Box align="center" justify="center" pad="large">
         <Box align="center" justify="center">
           <Card align="stretch" justify="center" direction="column" pad="large">
-            <Box align="stretch" justify="center" pad="xsmall" direction="column" gap="none">
-              <Text textAlign="center">
-                Follow steps
-              </Text>
+            <Box
+              align="stretch"
+              justify="center"
+              pad="xsmall"
+              direction="column"
+              gap="none"
+            >
+              <Text textAlign="center">Follow steps</Text>
             </Box>
-            <Box align="stretch" justify="center" pad="xsmall" direction="column" gap="none">
-              <Box align="center" justify="start" direction="row" pad="small" gap="medium">
+            <Box
+              align="stretch"
+              justify="center"
+              pad="xsmall"
+              direction="column"
+              gap="none"
+            >
+              <Box
+                align="center"
+                justify="start"
+                direction="row"
+                pad="small"
+                gap="medium"
+              >
                 <Spinner />
-                <Text>
-                  Approve Asset
-                </Text>
+                <Text>Approve Asset</Text>
               </Box>
             </Box>
-            <Box align="stretch" justify="center" pad="xsmall" direction="column" gap="none">
-              <Box align="start" justify="start" direction="row" gap="medium" pad="small">
+            <Box
+              align="stretch"
+              justify="center"
+              pad="xsmall"
+              direction="column"
+              gap="none"
+            >
+              <Box
+                align="start"
+                justify="start"
+                direction="row"
+                gap="medium"
+                pad="small"
+              >
                 <Spinner />
-                <Text>
-                  Purchase
-                </Text>
+                <Text>Purchase</Text>
               </Box>
             </Box>
-            <Box align="stretch" justify="center" pad="xsmall" direction="column" gap="none">
+            <Box
+              align="stretch"
+              justify="center"
+              pad="xsmall"
+              direction="column"
+              gap="none"
+            >
               <Button label="Cancel" disabled />
             </Box>
           </Card>
         </Box>
       </Box>
     </Box>
-  )
-}
-
+  );
+};
 
 export default function SignIn() {
-
   const address = useAddress();
   const connectWithMetamask = useMetamask();
   const disconnectWallet = useDisconnect();
@@ -196,8 +281,25 @@ export default function SignIn() {
 
   if (session && address) {
     return (
+
+
+      <ResponsiveContext.Consumer>
+      {(size) =>
+        size === "small" ? (
+          <Box direction="column">
+            <MintingApprove />
+      <MintingInterfaceItem />
+          </Box>
+        ) : (
+          <Box direction="row">
+            <MintingApprove />
+      <MintingInterfaceItem />
+          </Box>
+        )
+      }
+    </ResponsiveContext.Consumer>
+
       // <MintingApprove/>
-      <MintingInterfaceItem/>
       // <div className={styles.bigSpacerTop}>
       //   <Button
       //     onClick={() => signOut()}
@@ -215,39 +317,60 @@ export default function SignIn() {
   // 2. Connect with Discord (OAuth)
   if (!session) {
     return (
+
       <Box align="stretch" justify="center">
-      <Box align="center" justify="center" pad="large" direction="row" gap="small">
-        <Box align="start" justify="center" gap="small" pad="small">
-          <Text size="medium" textAlign="start">
-            amet consectetur 
-          </Text>
-          <Text size="xxlarge">
-            Lorem ipsum
-amet consectetur adipiscing elit.
-          </Text>
-        </Box>
-        <Box align="center" justify="center">
-          <Image src="https://photos.smugmug.com/Pinnacles-May-2019/n-8KLNDR/i-bxkrqwL/0/1c7fa7f2/M/i-bxkrqwL-M.jpg" fill="horizontal" fit="cover" />
-        </Box>
-        <Box align="start" justify="start" gap="small" pad="small">
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet elementum, laoreet egestas elit vitae 
-          </Text>
-          <Button label="Login with Discord" onClick={() => signIn()} />
+        <Box
+          align="center"
+          justify="center"
+          pad="large"
+          direction="row"
+          gap="small"
+        >
+          <Box align="start" justify="center" gap="small" pad="small">
+            <Text size="medium" textAlign="start">
+              amet consectetur
+            </Text>
+            <Text size="xxlarge">
+              Lorem ipsum amet consectetur adipiscing elit.
+            </Text>
+          </Box>
+          <Box align="center" justify="center">
+            <Image
+              src="https://photos.smugmug.com/Pinnacles-May-2019/n-8KLNDR/i-bxkrqwL/0/1c7fa7f2/M/i-bxkrqwL-M.jpg"
+              fill="horizontal"
+              fit="cover"
+            />
+          </Box>
+          <Box align="start" justify="start" gap="small" pad="small">
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet
+              elementum, laoreet egestas elit vitae
+            </Text>
+            <Button label="Login with Discord" onClick={() => signIn()} />
+          </Box>
         </Box>
       </Box>
-    </Box>
     );
   }
-
-  
 
   // 1. Connect with MetaMask
   if (!address) {
     return (
-<ConnectWallet/>
+      <ResponsiveContext.Consumer>
+        {(size) =>
+          size === "small" ? (
+            <Box direction="column">
+              <ConnectWallet />
+              <MintingApprove />
+            </Box>
+          ) : (
+            <Box direction="row">
+              <ConnectWallet />
+              <MintingApprove />
+            </Box>
+          )
+        }
+      </ResponsiveContext.Consumer>
     );
   }
 }
-
-
