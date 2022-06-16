201fbbecd37b1f0a4403200a868e82a94204a18c
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2022-06-16 17:02:05 +0200

    update alert

--- components/Airdrop.js
+++ components/Airdrop.js
@@ -103,7 +103,7 @@ export default function Airdrop(props) {
     }
     // If the user does not meet the criteria to have a signature generated, we can show them an error
     else {
-      alert("Something went wrong. Are you a member of the discord?");
+      alert("Something went wrong. Are you a member of our discord?");
     }
   }
 
