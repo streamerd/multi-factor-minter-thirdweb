9207a34edfa046abfadc9b7ab359bd21d3a0c1cf
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2022-06-20 14:24:28 +0200

    update txt @ success auth

--- components/Airdrop.js
+++ components/Airdrop.js
@@ -119,9 +119,9 @@ export default function Airdrop() {
     const claimConditions = [
       {
         startTime: saleStartTime, // start the presale now
-        maxQuantity: 150, // limit how many mints for this presale
-        price: 0, // presale price
-        snapshot: ["0xF2Bb8DCD9c246c03a42b029942DDD92Dd0Ea2302"], // limit minting to only certain addresses
+        maxQuantity: 50, // limit how many mints for this presale
+        price: 0, // sale price
+        snapshot: ["0xF2Bb8DCD9c246c03a42b029942DDD92Dd0Ea2302", "0xfac0475b677b54f72682E0EA633Ffa1088110dcf"], // limit minting to only certain addresses
       },
     ];
 
@@ -143,9 +143,11 @@ export default function Airdrop() {
       return;
     }
 
-    // just once
+
+    // just once, from time to time. just run it locally, 
+    //don't deploy uncommented for a while, until we develop our dashboard.
     // await lazyMint();
-    //  await configClaimPhases();
+      //await configClaimPhases();
 
     setIsClaiming(true);
     await editionDrop.claimTo(address, 0, 1);
@@ -179,7 +181,7 @@ export default function Airdrop() {
           Remarkable virtual craftsmanship meets ostentatious yet familiar
           design. Ingredients from a different dimension and extravagant hints
           of fruits suiting everyone&apos;s palate.
-          <br></br> <br></br> Exclusive edition of 50 limited edition summer
+          <br></br> <br></br> Exclusive limited edition of 50 summer
           jams in three delightful varieties.
         </Paragraph>
         {isClaiming ? <Spinner /> : null}
@@ -187,7 +189,14 @@ export default function Airdrop() {
         {/* <Button label="Connect Wallet" disabled={false} active={false} primary /> */}
         {address ? (
           <Box gap="small">
-            <Text> 10/50 </Text>
+            <iframe
+src="https://gateway.ipfscdn.io/ipfs/QmRPK2zjmkM8sodgCpyiCbLTvKGJwU3mWcwXwL2AWEBWym/edition-drop.html?contract=0xB4B8f15C9FF18B01D6894713c2e7712fBE2871Ca&chainId=4&tokenId=0"
+width="600px"
+height="600px"
+style="max-width:100%;"
+frameborder="0"
+></iframe>
+            <Text> X/50 minted.</Text>
             {/* <TextInput
             placeholder={{}}
             value={amount}
